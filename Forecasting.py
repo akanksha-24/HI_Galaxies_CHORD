e599fc28142d25bc392a353399a40093b890dd5e
xpertdispatch <ashwani@rogers.com>
2025-11-26 19:49:31 -0500

    5 and 10km/s runs

--- Forecasting.py
+++ Forecasting.py
@@ -53,7 +53,7 @@ def detections_fromRMS(catalog_file, maskFl='', RMS=0.1, sigma=6):
 def detections_fromObs(catalog_file, maskFl, obsyears, nstrips, sigma=6, chan_width_kms=None):
     obsdays = 365*obsyears/nstrips
     if chan_width_kms!=None:
-        chan_width_Hz = gf.chanwidth_vel2freq(10, f_rest=1420)
+        chan_width_Hz = gf.chanwidth_vel2freq(chan_width_kms, f_rest=1420)
         print("chan width in Hz is ", chan_width_Hz)
     else:
         chan_width_Hz = gf.chan_width
@@ -153,7 +153,7 @@ if __name__ == "__main__":
 #    detections_ALFALFA(catalog_file='catalogs_output/VolLim_20to80deg_Dmax200_MHImatchmocksim.npy_rank0.npy', maskFl='DetectionsALFALFA_Vollim_Matchsim_20to80deg_Dmax200.npy')
 #   detections_ALFALFA(catalog_file='catalogs_output/MockAlf_D200_Dec20to80_changeVelocity.npy', maskFl='DetectionsALFALFA_MockSim_changeVelocity_20to80deg_Dmax200.npy')
 #    SNRint_fromFile('catalogs_output/MockAlf_FullSky.npy', RMS=1, plt=False, maskFl='catalogs_output/maskRMS1_sigma6_MockAlf_FullSky.npy')
-    detections_fromObs('catalogs_output/VolLim_20to80deg_zmax0p8_rank3.npy', obsyears=1, nstrips=20, chan_width_kms=5, 
+    detections_fromObs('catalogs_output/VolLim_20to80deg_zmax0p8_rank3.npy', obsyears=1, nstrips=20, chan_width_kms=10, 
                   maskFl='catalogs_output/Detected1yr_RMS0p18_VolLim_20to80deg_zmax0p8_chan5kms_rank3.npy')
 #   SNRint_fromFile('catalogs_output/VolLim_20to60deg_zmin0p4_zmax1_MHI9to12.npy_rank1.npy', RMS=0.08, plt=False, 
 #                  maskFl='catalogs_output/Detected_VolLim_RMS0p08_20to80deg_z0p4to1_MHI9to12_new.npy')
